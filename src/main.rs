4850be5abd4a5a7218f37fc11f0db02b86e28d53
Michal Konopka <konopek68@gmail.com>
2023-11-14 22:54:12 +0100

    next

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value, &str>
                 Some('i') => {
                     return Ok(serde_json::Value::Number(
                         chars
-                            .take_while(|c| c == &'e')
+                            .take_while(|c| c != &'e')
                             .collect::<String>()
                             .parse::<i32>()
                             .unwrap()
