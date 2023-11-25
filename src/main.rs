5e9f2552024ecaad68a0f3faa017ddfbf41c92a9
Michal Konopka <konopek68@gmail.com>
2023-11-25 20:16:06 +0100

    next

--- src/main.rs
+++ src/main.rs
@@ -17,55 +17,55 @@ fn main() {
         print!("{}", info);
     } else if command == "peers" {
         let info = get_metafile_info(&args);
-
-        let info_hash_encoded: String = unsafe { String::from_utf8_unchecked(info.hash) };
-
-        let response = reqwest::blocking::Client::new()
-            .get(info.trackter_url)
-            .query(&[
-                ("info_hash", info_hash_encoded.as_str()),
-                ("peer_id", "00112233445566778899"),
-                ("port", "6881"),
-                ("uploaded", "0"),
-                ("downloaded", "0"),
-                ("left", &info.length.to_string()),
-                ("compact", "1"),
-            ])
-            .send()
-            .unwrap();
-
-        let body = response.bytes().unwrap();
-
-        let iterator = &mut body.iter().copied();
-
-        let value = decode_bencoded_value(iterator).unwrap();
-
-        let encoded_peers = value.as_object().unwrap()["peers"]
-            .as_str()
-            .expect("peers can be parse to string")
-            .as_bytes()
-            .chunks(6);
-
-        let mut peers = Vec::new();
-        for encoded_peer in encoded_peers {
-            let mut iterator = encoded_peer.iter();
-            let first_octet = iterator.next().unwrap();
-            let second_octet = iterator.next().unwrap();
-            let third_octet = iterator.next().unwrap();
-            let fourth_octet = iterator.next().unwrap();
-
-            let first_byte_port = *iterator.next().unwrap() as u16;
-            let second_byte_port = *iterator.next().unwrap() as u16;
-            let port = (first_byte_port << 8) | second_byte_port;
-
-            let peer_address = format!(
-                "{}.{}.{}.{}:{}",
-                first_octet, second_octet, third_octet, fourth_octet, port
-            );
-            peers.push(peer_address);
-        }
-        print!("{:?}", peers);
+        let peers = discover_peers(info).unwrap();
+        println!("{:?}", peers);
     } else {
         println!("unknown command: {}", args[1])
     }
+}
+
+fn discover_peers(info: bittorrent_starter_rust::MetaInfoFile) -> Option<Vec<String>> {
+    let info_hash_encoded: String = unsafe { String::from_utf8_unchecked(info.hash) };
+    let response = reqwest::blocking::Client::new()
+        .get(info.trackter_url)
+        .query(&[
+            ("info_hash", info_hash_encoded.as_str()),
+            ("peer_id", "00112233445566778899"),
+            ("port", "6881"),
+            ("uploaded", "0"),
+            ("downloaded", "0"),
+            ("left", &info.length.to_string()),
+            ("compact", "1"),
+        ])
+        .send()
+        .unwrap();
+    let body = response.bytes().unwrap();
+    let iterator = &mut body.iter().copied();
+    let value = decode_bencoded_value(iterator).unwrap();
+    let encoded_peers = value.as_object().unwrap()["peers"]
+        .as_str()
+        .expect("peers can be parse to string")
+        .as_bytes()
+        .chunks(6);
+    let mut peers = Vec::new();
+    for encoded_peer in encoded_peers {
+        let mut iterator = encoded_peer.iter();
+        let first_octet = iterator.next().unwrap();
+        let second_octet = iterator.next().unwrap();
+        let third_octet = iterator.next().unwrap();
+        let fourth_octet = iterator.next().unwrap();
+
+        let first_byte_port = *iterator.next().unwrap() as u16;
+        let second_byte_port = *iterator.next().unwrap() as u16;
+        let port = (first_byte_port << 8) | second_byte_port;
+
+        let peer_address = format!(
+            "{}.{}.{}.{}:{}",
+            first_octet, second_octet, third_octet, fourth_octet, port
+        );
+        peers.push(peer_address);
+    }
+    return Some(peers);
+
+    None
 }
