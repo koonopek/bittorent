ea2ead9fc4780f43ab2ffbb0ccef22c688c3b200
Michal Konopka <konopek68@gmail.com>
2023-11-25 13:28:14 +0100

    next

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
     } else if command == "peers" {
         let info = get_metafile_info(&args);
 
-        let info_hash_encoded: String = info.hash.iter().map(|&b| format!("%{:02X}", b)).collect();
+        let info_hash_encoded: String = unsafe { String::from_utf8_unchecked(info.hash) };
 
         let response = reqwest::blocking::Client::new()
             .get(info.trackter_url)
