dc5f4f6f7289b3e2555e278656993ab8cecf9ce4
Michal Konopka <konopek68@gmail.com>
2023-12-03 10:54:09 +0100

    next

--- src/main.rs
+++ src/main.rs
@@ -61,7 +61,7 @@ fn main() {
 
         let payload_size = u32::from_be_bytes(payload_size_buf) as usize;
 
-        let mut payload = Vec::with_capacity(payload_size);
+        let mut payload = vec![0; payload_size];
 
         connection
             .tcp_stream
